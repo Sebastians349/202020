7c4685e28b53f94055e3cdf97ab22e06940bc408
Sebastian Simarro <ssebastian210@gmail.com>
2021-08-13 10:22:58 -0300

    - css => circular progress

--- src/components/Reloj.js
+++ src/components/Reloj.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Box, Circle } from '@chakra-ui/react';
-
+import { Box, Circle, CircularProgress } from '@chakra-ui/react';
 const Reloj = () => {
   return (
     <Box>
-      <Circle size="20rem" bg="brand.900" border="" color="white">
-        ddddddd
+      <Circle
+        size="20rem"
+        bg="transparent"
+        borderWidth=".5rem"
+        borderColor="black.500"
+        color="black.500"
+      >
+        <CircularProgress value={40} size="xs" />
       </Circle>
     </Box>
   );
